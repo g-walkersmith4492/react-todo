b887fb329b0ce86d0bae823eced67d8b4a85e102
g-walkersmith4492 <g.walkersmith4492@gmail.com>
2019-03-02 12:51:58 -0500

    Updates

---
+++ precache-manifest.603f45e783e9f774b230ecdfc668019e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "713502522859ff23d42e",
+    "url": "/react-todo/static/js/runtime~main.71350252.js"
+  },
+  {
+    "revision": "c8b444fa6e9396fd0e12",
+    "url": "/react-todo/static/js/main.c8b444fa.chunk.js"
+  },
+  {
+    "revision": "05bd70c319adf4914028",
+    "url": "/react-todo/static/js/2.05bd70c3.chunk.js"
+  },
+  {
+    "revision": "c8b444fa6e9396fd0e12",
+    "url": "/react-todo/static/css/main.1effbc99.chunk.css"
+  },
+  {
+    "revision": "c121300a079ea8111851417902609d99",
+    "url": "/react-todo/index.html"
+  }
+];
